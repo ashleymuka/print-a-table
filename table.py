9898ae9c76e36e649c0ef3af9c84f392f7a2e5ba
ashleymuka <153102801+ashleymuka@users.noreply.github.com>
2023-12-12 20:55:05 -0600

    Add files via upload

---
+++ table.py
@@ -0,0 +1,25 @@
+'''
+
+Author: Ashley Muka
+Assignment Title: Program 2
+Assignment Description: Print a table
+Due Date:06/05/2023
+Date Created:06/03/2023
+Date Last Modified:06/03/2023
+
+'''
+
+#input
+#process
+#output
+
+print("A        B        A^B")
+print("1        0        1")
+print("2        1        2")
+print("3        2        9")
+print("4        3        64")
+print("5        4        625")
+
+
+
+
