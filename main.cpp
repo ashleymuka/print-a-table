d5ceb315f6bf5b3b94574f0c194b4c2ea6fa0455
ashleymuka <153102801+ashleymuka@users.noreply.github.com>
2023-12-12 21:31:42 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+// Author: Ashley Muka
+// Assignment Title: Program 2
+// Assignment Description:create table
+// Due Date:1/20/2023
+// Date Created:1/20/2023
+// Date Last Modified:1/20/2023
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "A        B        A^B" << endl;
+    cout << "1        0        1" << endl;
+    cout << "2        1        2" << endl;
+    cout << "3        2        9" << endl;
+    cout << "4        3        64" << endl;
+    cout << "5        4        625" << endl;
+    return 0;
+}
